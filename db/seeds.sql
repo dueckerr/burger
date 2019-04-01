0a2f1a55e0fac22710513be34ae65041cdc914f2
Reagan D Duecker <dueckerr@gmail.com>
2019-03-31 22:51:42 -0500

    1

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royal with cheese ', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Kahuna', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krusty Burger', false);
